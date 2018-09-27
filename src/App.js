a986019ce9907bd2eee3c788ed9c386fd654a777
Tyler Maynard <tyler@saltcitydigital.com>
2018-09-27 00:08:21 -0600

    Module 2 Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 import Quiz from './Quiz'
+import QuizQuestion from './QuizQuestion'
 
 class App extends Component {
   render() {
     return (
-      <Quiz />
+      <div>
+        <Quiz />
+      </div>
     )
   }
 }
